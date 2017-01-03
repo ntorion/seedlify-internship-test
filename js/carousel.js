3343a5dc42407cbaffb938df7a7e66da5e2b6a8f
Nicolas Torion <nicolas.torion@insa-rouen.fr>
2017-01-03 15:44:15 +0100

    first commit of files

---
+++ js/carousel.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+
+  var selected = "#item1";
+
+  var pos = $(selected).position();
+  $("#selection").css({top: pos.top + 'px', left: pos.left + 'px'});
+
+  description = $("#description-item1").html();
+  $('.inside').html(description);
+
+  update(selected);
+
+  function update(selected) {
+    var w = $(selected).outerWidth();
+    var h = $(selected).outerHeight();
+    var pos2 = $(selected).position();
+
+    $("#selection").css({'top': pos2.top + 'px', 'left': pos2.left + 'px'});
+
+    $("#selection").css({width: w + 'px', height: h + 'px'});
+  }
+
+  $('.product-selection').click(function() {
+    selected = "#" + $(this).attr("id");
+
+    srcImg = $(selected + " img").attr("src");
+    $('.product-image img').attr("src", srcImg).fadeIn("slow");
+
+    descriptionID = "#description-" + $(this).attr("id");
+    description = $(descriptionID).html();
+    $('.inside').html(description).fadeIn("slow");
+
+    $("#selection").css({'top': $(selected).position().top + 'px'});
+    $("#selection").animate({'left': $(selected).position().left + 'px'}, 200, function(){});
+
+  });
+
+  $( window ).resize(function() {
+    update(selected);
+  });
+
+});

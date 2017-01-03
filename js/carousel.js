d22ff846b5a4a270d023bfaa31ba4e6e049b8efa
Nicolas Torion <nicolas.torion@insa-rouen.fr>
2017-01-03 23:06:56 +0100

    update js

--- js/carousel.js
+++ js/carousel.js
@@ -2,9 +2,6 @@ $( document ).ready(function() {
 
   var selected = "#item1";
 
-  var pos = $(selected).position();
-  $("#selection").css({top: pos.top + 'px', left: pos.left + 'px'});
-
   description = $("#description-item1").html();
   $('.inside').html(description);
 
@@ -15,20 +12,19 @@ $( document ).ready(function() {
     var h = $(selected).outerHeight();
     var pos2 = $(selected).position();
 
-    $("#selection").css({'top': pos2.top + 'px', 'left': pos2.left + 'px'});
-
     $("#selection").css({width: w + 'px', height: h + 'px'});
+    $("#selection").css({'top': pos2.top + 'px', 'left': pos2.left + 'px'});
   }
 
   $('.product-selection').click(function() {
     selected = "#" + $(this).attr("id");
 
     srcImg = $(selected + " img").attr("src");
-    $('.product-image img').attr("src", srcImg).fadeIn("slow");
+    $('.product-image img').attr("src", srcImg);
 
     descriptionID = "#description-" + $(this).attr("id");
     description = $(descriptionID).html();
-    $('.inside').html(description).fadeIn("slow");
+    $('.inside').html(description);
 
     $("#selection").css({'top': $(selected).position().top + 'px'});
     $("#selection").animate({'left': $(selected).position().left + 'px'}, 200, function(){});
